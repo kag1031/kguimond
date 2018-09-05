9492015a94eb7f7afce630ca00510e13f13d626b
Kimberly Guimond <sKimberlyGuimond@yccc.edu>
2018-09-05 19:20:11 -0400

    First commit

---
+++ src/dayone/HelloWorld.java
@@ -0,0 +1,17 @@
+package dayone;
+
+public class HelloWorld 
+{
+	// An object is 2 things.	
+	// 1. Actions. Methods. 
+	// 2. Attributes. Variables. 
+	private String name = "Mike";
+	
+	// main method. Starts the program. 
+	public static void main(String[] args) 
+	{
+	  //Prints to the console. 
+		System.out.println("Hello World");
+		System.out.println("My First Java App!!!");
+	}
+}
